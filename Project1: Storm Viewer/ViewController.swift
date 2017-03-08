fc129fdde648c9e344d8f91e771e573ff2088f0c
Jack Carter <DemGamingBulldogs@gmail.com>
2017-03-08 11:34:02 +0000

    Initial Commit

---
+++ Project1: Storm Viewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project1: Storm Viewer
+//
+//  Created by Jack Carter on 08/03/2017.
+//  Copyright © 2017 Jack Carter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
