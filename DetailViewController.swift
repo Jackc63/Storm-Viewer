b4a9d12fd993c64d6df742ab3b7909da063ec441
Jack Carter <DemGamingBulldogs@gmail.com>
2017-03-08 15:57:18 +0000

    initial commit

---
+++ DetailViewController.swift
@@ -0,0 +1,39 @@
+//
+//  DetailviewControllerViewController.swift
+//  Project1: Storm Viewer
+//
+//  Created by Jack Carter on 08/03/2017.
+//  Copyright © 2017 Jack Carter. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = selectedImage
+
+        if let imageToLoad = selectedImage {
+            imageView.image  = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
